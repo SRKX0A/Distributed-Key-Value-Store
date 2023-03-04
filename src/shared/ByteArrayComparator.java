d0119260401ac383d9d7861a1a834391c8ae28ba
SRKX0A <44185385+SRKX0A@users.noreply.github.com>
2023-03-04 04:06:49 -0500

    EXPERIMENTAL TESTER: use BigIntegers for comparison

--- src/shared/ByteArrayComparator.java
+++ src/shared/ByteArrayComparator.java
@@ -2,19 +2,16 @@ package shared;
 
 import java.io.*;
 import java.util.*;
+import java.math.*;
 
 public class ByteArrayComparator implements Serializable, Comparator<byte[]> {
 
     public int compare(byte[] b1, byte[] b2) {
-	for (int i = 0; i < b1.length; i++) {
-	if (b1[i] < b2[i]) {
-		return -1;
-	    } else if (b1[i] > b2[i]) {
-		return 1;
-	    }
-	}
 
-	return 0;
+	BigInteger bigInt1 = new BigInteger(1, b1);
+	BigInteger bigInt2 = new BigInteger(1, b2);
+
+	return bigInt1.compareTo(bigInt2);
     }
 
 }
